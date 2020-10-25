5b66d966f0e449a378340b3bb72a78cf01a91788
Robert Pelka <123455ff@gmail.com>
2020-10-25 21:47:44 +0100

    Changed some assets and added plane.

--- flappyBert.py
+++ flappyBert.py
@@ -9,21 +9,33 @@ pygame.display.set_caption("Flappy Bert")
 screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()
 exitPressed = False
+gravity = 0.25
+robert_movement = 0
 
-bg_surface = pygame.image.load("assets/background-day.png").convert()
+bg_surface = pygame.image.load("assets/background.png").convert()
 floor_surface = pygame.image.load("assets/base.png").convert()
 floor_x_pos = 0
+
+robert_surface = pygame.image.load("assets/robert.png").convert_alpha()
+robert_rect = robert_surface.get_rect(center = (50, 256))
 
 while not exitPressed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exitPressed = True
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                robert_movement = 0
+                robert_movement -= 6
     
     if exitPressed:
         break
 
     screen.blit(bg_surface, (0, 0))
+    screen.blit(robert_surface, robert_rect)
+    robert_movement += gravity
+    robert_rect.centery += robert_movement
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -288:
