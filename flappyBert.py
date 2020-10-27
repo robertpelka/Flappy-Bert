4f2c4bc2d38007874825d9b54f316f08b0fd0ee0
Robert Pelka <123455ff@gmail.com>
2020-10-27 13:40:52 +0100

    Added pipes and some minor changes.

--- flappyBert.py
+++ flappyBert.py
@@ -1,8 +1,28 @@
 import pygame
+import random
 
 def draw_floor():
-    screen.blit(floor_surface, (floor_x_pos, 400))
-    screen.blit(floor_surface, (floor_x_pos+288, 400))
+    screen.blit(floorSurface, (floorX, 440))
+    screen.blit(floorSurface, (floorX+288, 440))
+
+def create_pipe():
+    randomHeight = random.choice(pipeHeights)
+    bottomPipe = pipeSurface.get_rect(midtop = (288, randomHeight))
+    topPipe = pipeSurface.get_rect(midbottom = (288, randomHeight-150))
+    return bottomPipe, topPipe
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 5
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.top < 0:
+            flippedPipe = pygame.transform.flip(pipeSurface, False, True)
+            screen.blit(flippedPipe, pipe)
+        else:
+            screen.blit(pipeSurface, pipe)
 
 pygame.init()
 pygame.display.set_caption("Flappy Bert")
@@ -10,14 +30,17 @@ screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()
 exitPressed = False
 gravity = 0.25
-robert_movement = 0
+robertMovement = 0
+floorX = 0
 
-bg_surface = pygame.image.load("assets/background.png").convert()
-floor_surface = pygame.image.load("assets/base.png").convert()
-floor_x_pos = 0
-
-robert_surface = pygame.image.load("assets/robert.png").convert_alpha()
-robert_rect = robert_surface.get_rect(center = (50, 256))
+bgSurface = pygame.image.load("assets/background.png").convert()
+floorSurface = pygame.image.load("assets/base.png").convert()
+robertSurface = pygame.image.load("assets/robert.png").convert_alpha()
+robertRect = robertSurface.get_rect(center = (50, 256))
+pipeSurface = pygame.image.load("assets/pipe.png").convert_alpha()
+pipeList = []
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1200)
 
 while not exitPressed:
     for event in pygame.event.get():
@@ -26,21 +49,31 @@ while not exitPressed:
             pygame.quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                robert_movement = 0
-                robert_movement -= 6
-    
+                robertMovement = 0
+                robertMovement -= 6
+        if event.type == SPAWNPIPE:
+            pipeList.extend(create_pipe())
     if exitPressed:
         break
 
-    screen.blit(bg_surface, (0, 0))
-    screen.blit(robert_surface, robert_rect)
-    robert_movement += gravity
-    robert_rect.centery += robert_movement
-    floor_x_pos -= 1
+    # Background
+    screen.blit(bgSurface, (0, 0))
+
+    # Pipes
+    pipeList = move_pipes(pipeList)
+    draw_pipes(pipeList)
+    pipeHeights = [200, 250, 300]
+
+    # Floor
+    floorX -= 1
     draw_floor()
-    if floor_x_pos <= -288:
-        floor_x_pos = 0
-    
+    if floorX <= -288:
+        floorX = 0
+
+    # Plane 
+    screen.blit(robertSurface, robertRect)
+    robertMovement += gravity
+    robertRect.centery += robertMovement
 
     pygame.display.update()
     clock.tick(120)
