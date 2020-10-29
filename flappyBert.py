d99de5ed734a8d14bf0a1ab46d07fcc8cc6f1ae1
Robert Pelka <123455ff@gmail.com>
2020-10-29 23:44:25 +0100

    Added collisions.

--- flappyBert.py
+++ flappyBert.py
@@ -24,11 +24,23 @@ def draw_pipes(pipes):
         else:
             screen.blit(pipeSurface, pipe)
 
+def check_colisions(pipes):
+    if robertRect.bottom >= 440:
+        return False
+    for pipe in pipes:
+        if pipe.colliderect(robertRect):
+            return False
+    return True
+
+
 pygame.init()
 pygame.display.set_caption("Flappy Bert")
 screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()
+
+isGameActive = True
 exitPressed = False
+
 gravity = 0.25
 robertMovement = 0
 floorX = 0
@@ -38,9 +50,11 @@ floorSurface = pygame.image.load("assets/base.png").convert()
 robertSurface = pygame.image.load("assets/robert.png").convert_alpha()
 robertRect = robertSurface.get_rect(center = (50, 256))
 pipeSurface = pygame.image.load("assets/pipe.png").convert_alpha()
+
 pipeList = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
+
 
 while not exitPressed:
     for event in pygame.event.get():
@@ -48,9 +62,14 @@ while not exitPressed:
             exitPressed = True
             pygame.quit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and isGameActive:
                 robertMovement = 0
                 robertMovement -= 6
+            if event.key == pygame.K_SPACE and not isGameActive:
+                pipeList = []
+                robertRect.center = (50, 256)
+                robertMovement = 0
+                isGameActive = True
         if event.type == SPAWNPIPE:
             pipeList.extend(create_pipe())
     if exitPressed:
@@ -59,21 +78,24 @@ while not exitPressed:
     # Background
     screen.blit(bgSurface, (0, 0))
 
-    # Pipes
-    pipeList = move_pipes(pipeList)
-    draw_pipes(pipeList)
-    pipeHeights = [200, 250, 300]
+    if isGameActive:
+        # Pipes
+        pipeList = move_pipes(pipeList)
+        draw_pipes(pipeList)
+        pipeHeights = [200, 250, 300]
+
+        # Plane 
+        screen.blit(robertSurface, robertRect)
+        robertMovement += gravity
+        robertRect.centery += robertMovement
+
+        isGameActive = check_colisions(pipeList)
 
     # Floor
     floorX -= 1
     draw_floor()
     if floorX <= -288:
         floorX = 0
-
-    # Plane 
-    screen.blit(robertSurface, robertRect)
-    robertMovement += gravity
-    robertRect.centery += robertMovement
 
     pygame.display.update()
     clock.tick(120)
