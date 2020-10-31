65e693256ee2539376d254a714be05c271f880a8
robertpelka <123455ff@gmail.com>
2020-10-31 23:49:34 +0100

    Plane animation added.

--- flappyBert.py
+++ flappyBert.py
@@ -1,101 +1,132 @@
-import pygame
 import random
+import pygame
 
 def draw_floor():
-    screen.blit(floorSurface, (floorX, 440))
-    screen.blit(floorSurface, (floorX+288, 440))
+    screen.blit(floor_surface, (floor_x, 440))
+    screen.blit(floor_surface, (floor_x+288, 440))
 
 def create_pipe():
-    randomHeight = random.choice(pipeHeights)
-    bottomPipe = pipeSurface.get_rect(midtop = (288, randomHeight))
-    topPipe = pipeSurface.get_rect(midbottom = (288, randomHeight-150))
-    return bottomPipe, topPipe
+    random_height = random.choice(pipe_heights)
+    bottom_pipe = pipe_surface.get_rect(midtop = (288, random_height))
+    top_pipe = pipe_surface.get_rect(midbottom = (288, random_height-150))
+    return bottom_pipe, top_pipe
 
 def move_pipes(pipes):
     for pipe in pipes:
-        pipe.centerx -= 5
+        pipe.centerx -= 3
     return pipes
 
 def draw_pipes(pipes):
     for pipe in pipes:
         if pipe.top < 0:
-            flippedPipe = pygame.transform.flip(pipeSurface, False, True)
-            screen.blit(flippedPipe, pipe)
+            flipped_pipe = pygame.transform.flip(pipe_surface, False, True)
+            screen.blit(flipped_pipe, pipe)
         else:
-            screen.blit(pipeSurface, pipe)
+            screen.blit(pipe_surface, pipe)
 
 def check_colisions(pipes):
-    if robertRect.bottom >= 440:
+    if robert_rect.bottom >= 440:
         return False
     for pipe in pipes:
-        if pipe.colliderect(robertRect):
+        if pipe.colliderect(robert_rect):
             return False
     return True
 
+def rotate_robert(robert):
+    new_robert = pygame.transform.rotozoom(robert, -robert_movement*2, 1)
+    return new_robert
+
+def display_score(is_game_active):
+    if is_game_active:
+        score_surface = game_font.render(str(score), False, (255, 255, 255))
+        score_rect = score_surface.get_rect(center = (144, 50))
+        screen.blit(score_surface, score_rect)
+    else:
+        score_surface = game_font.render(str(score), False, (255, 255, 255))
+        score_rect = score_surface.get_rect(center = (144, 50))
+        screen.blit(score_surface, score_rect)
+        score_surface = game_font.render("Best: "+str(high_score), False, (255, 255, 255))
+        score_rect = score_surface.get_rect(center = (144, 90))
+        screen.blit(score_surface, score_rect)
+
+def update_high_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
 
 pygame.init()
 pygame.display.set_caption("Flappy Bert")
 screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()
 
-isGameActive = True
-exitPressed = False
+is_game_active = True
+exit_pressed = False
 
-gravity = 0.25
-robertMovement = 0
-floorX = 0
+gravity = 0.15
+robert_movement = 0
+floor_x = 0
 
-bgSurface = pygame.image.load("assets/background.png").convert()
-floorSurface = pygame.image.load("assets/base.png").convert()
-robertSurface = pygame.image.load("assets/robert.png").convert_alpha()
-robertRect = robertSurface.get_rect(center = (50, 256))
-pipeSurface = pygame.image.load("assets/pipe.png").convert_alpha()
+score = 0
+high_score = 0
 
-pipeList = []
+game_font = pygame.font.Font("assets/font.ttf",32)
+
+bg_surface = pygame.image.load("assets/background.png").convert()
+floor_surface = pygame.image.load("assets/base.png").convert()
+robert_surface = pygame.image.load("assets/robert.png").convert_alpha()
+robert_rect = robert_surface.get_rect(center = (50, 256))
+pipe_surface = pygame.image.load("assets/pipe.png").convert_alpha()
+
+pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
 
 
-while not exitPressed:
+while not exit_pressed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exitPressed = True
+            exit_pressed = True
             pygame.quit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and isGameActive:
-                robertMovement = 0
-                robertMovement -= 6
-            if event.key == pygame.K_SPACE and not isGameActive:
-                pipeList = []
-                robertRect.center = (50, 256)
-                robertMovement = 0
-                isGameActive = True
+            if event.key == pygame.K_SPACE and is_game_active:
+                robert_movement = 0
+                robert_movement -= 5
+            if event.key == pygame.K_SPACE and not is_game_active:
+                pipe_list = []
+                robert_rect.center = (50, 256)
+                robert_movement = 0
+                score = 0
+                is_game_active = True
         if event.type == SPAWNPIPE:
-            pipeList.extend(create_pipe())
-    if exitPressed:
+            pipe_list.extend(create_pipe())
+    if exit_pressed:
         break
 
     # Background
-    screen.blit(bgSurface, (0, 0))
+    screen.blit(bg_surface, (0, 0))
 
-    if isGameActive:
+    if is_game_active:
         # Pipes
-        pipeList = move_pipes(pipeList)
-        draw_pipes(pipeList)
-        pipeHeights = [200, 250, 300]
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
+        pipe_heights = [200, 250, 300]
 
-        # Plane 
-        screen.blit(robertSurface, robertRect)
-        robertMovement += gravity
-        robertRect.centery += robertMovement
+        # Plane
+        rotated_robert = rotate_robert(robert_surface)
+        screen.blit(rotated_robert, robert_rect)
+        robert_movement += gravity
+        robert_rect.centery += robert_movement
 
-        isGameActive = check_colisions(pipeList)
+        is_game_active = check_colisions(pipe_list)
+
+    update_high_score(score, high_score)
+    display_score(is_game_active)
 
     # Floor
-    floorX -= 1
+    floor_x -= 1
     draw_floor()
-    if floorX <= -288:
-        floorX = 0
+    if floor_x <= -288:
+        floor_x = 0
 
     pygame.display.update()
     clock.tick(120)
