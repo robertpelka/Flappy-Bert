ef1253a2d7df4aa51ce1a5dc0d96a1dab44cd0e3
robertpelka <123455ff@gmail.com>
2020-11-03 21:20:11 +0100

    Added score counting.

--- flappyBert.py
+++ flappyBert.py
@@ -7,8 +7,8 @@ def draw_floor():
 
 def create_pipe():
     random_height = random.choice(pipe_heights)
-    bottom_pipe = pipe_surface.get_rect(midtop = (288, random_height))
-    top_pipe = pipe_surface.get_rect(midbottom = (288, random_height-150))
+    bottom_pipe = pipe_surface.get_rect(topleft = (288, random_height))
+    top_pipe = pipe_surface.get_rect(bottomleft = (288, random_height-150))
     return bottom_pipe, top_pipe
 
 def move_pipes(pipes):
@@ -49,6 +49,12 @@ def display_score(is_game_active):
         score_rect = score_surface.get_rect(center = (144, 90))
         screen.blit(score_surface, score_rect)
 
+def is_pipe_passed(pipe_position):
+    if pipe_position < 50:
+        return True
+    else:
+        return False
+
 def update_high_score(score, high_score):
     if score > high_score:
         high_score = score
@@ -78,8 +84,11 @@ robert_rect = robert_surface.get_rect(center = (50, 256))
 pipe_surface = pygame.image.load("assets/pipe.png").convert_alpha()
 
 pipe_list = []
+pipe_heights = [200, 250, 300]
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
+pipe_number = 0
+pipe_to_pass_position = 1000
 
 
 while not exit_pressed:
@@ -93,12 +102,16 @@ while not exit_pressed:
                 robert_movement -= 5
             if event.key == pygame.K_SPACE and not is_game_active:
                 pipe_list = []
+                pipe_number = 0
+                pipe_to_pass_position = 288
                 robert_rect.center = (50, 256)
                 robert_movement = 0
                 score = 0
                 is_game_active = True
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
+            pipe_to_pass_position = pipe_list[pipe_number].centerx
+            pipe_number +=2
     if exit_pressed:
         break
 
@@ -108,25 +121,31 @@ while not exit_pressed:
     if is_game_active:
         # Pipes
         pipe_list = move_pipes(pipe_list)
-        draw_pipes(pipe_list)
-        pipe_heights = [200, 250, 300]
+        pipe_to_pass_position -= 3
+        if is_pipe_passed(pipe_to_pass_position):
+            score += 1 
+            pipe_to_pass_position = 1000
 
         # Plane
         rotated_robert = rotate_robert(robert_surface)
-        screen.blit(rotated_robert, robert_rect)
         robert_movement += gravity
         robert_rect.centery += robert_movement
 
         is_game_active = check_colisions(pipe_list)
 
-    update_high_score(score, high_score)
+        # Floor
+        floor_x -= 1
+        if floor_x <= -288:
+            floor_x = 0
+
+    # Draw elements
+    draw_pipes(pipe_list)
+    screen.blit(rotated_robert, robert_rect)
+    draw_floor()
+
+    high_score = update_high_score(score, high_score)
     display_score(is_game_active)
 
-    # Floor
-    floor_x -= 1
-    draw_floor()
-    if floor_x <= -288:
-        floor_x = 0
 
     pygame.display.update()
     clock.tick(120)
