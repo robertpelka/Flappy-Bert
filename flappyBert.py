9a24472bff2b7f727be3bcddf73457d8f6453c3f
robertpelka <123455ff@gmail.com>
2020-11-08 23:54:29 +0100

    Added some texts and sounds.

--- flappyBert.py
+++ flappyBert.py
@@ -26,9 +26,11 @@ def draw_pipes(pipes):
 
 def is_any_collision(pipes):
     if robert_rect.bottom >= 440 or robert_rect.bottom < -360:
+        hit_sound.play()
         return True
     for pipe in pipes:
         if pipe.colliderect(robert_rect):
+            hit_sound.play()
             return True
     return False
 
@@ -38,14 +40,14 @@ def rotate_robert(robert):
 
 def display_score(is_game_active):
     if is_game_active:
-        score_surface = game_font.render(str(score), False, (255, 255, 255))
+        score_surface = medium_font.render(str(score), False, (255, 255, 255))
         score_rect = score_surface.get_rect(center = (144, 50))
         screen.blit(score_surface, score_rect)
     else:
-        score_surface = game_font.render(str(score), False, (255, 255, 255))
+        score_surface = medium_font.render(str(score), False, (255, 255, 255))
         score_rect = score_surface.get_rect(center = (144, 50))
         screen.blit(score_surface, score_rect)
-        score_surface = game_font.render("Best: "+str(high_score), False, (255, 255, 255))
+        score_surface = medium_font.render("Best: "+str(high_score), False, (255, 255, 255))
         score_rect = score_surface.get_rect(center = (144, 90))
         screen.blit(score_surface, score_rect)
 
@@ -60,6 +62,7 @@ def update_high_score(score, high_score):
         high_score = score
     return high_score
 
+pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
 pygame.init()
 screen = pygame.display.set_mode((288, 512))
 pygame.display.set_caption("Flappy Bert")
@@ -67,7 +70,7 @@ icon = pygame.image.load("assets/icon.png").convert_alpha()
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
-is_game_active = True
+is_game_active = False
 exit_pressed = False
 
 gravity = 0.15
@@ -77,13 +80,19 @@ floor_x = 0
 score = 0
 high_score = 0
 
-game_font = pygame.font.Font("assets/font.ttf",32)
+large_font = pygame.font.Font("assets/font.ttf",42)
+medium_font = pygame.font.Font("assets/font.ttf",32)
+small_font = pygame.font.Font("assets/font.ttf",24)
 
 bg_surface = pygame.image.load("assets/background.png").convert()
 floor_surface = pygame.image.load("assets/base.png").convert()
 robert_surface = pygame.image.load("assets/robert.png").convert_alpha()
-robert_rect = robert_surface.get_rect(center = (50, 256))
+robert_rect = robert_surface.get_rect(center = (-50, 256))
+rotated_robert = robert_surface
 pipe_surface = pygame.image.load("assets/pipe.png").convert_alpha()
+
+point_sound = pygame.mixer.Sound("sound/sfx_point.wav")
+hit_sound = pygame.mixer.Sound("sound/sfx_hit.wav")  
 
 pipe_list = []
 pipe_heights = [200, 250, 300]
@@ -105,7 +114,7 @@ while not exit_pressed:
             if event.key == pygame.K_SPACE and not is_game_active:
                 pipe_list = []
                 pipe_number = 0
-                pipe_to_pass_position = 288
+                pipe_to_pass_position = 1000
                 robert_rect.center = (50, 256)
                 robert_movement = 0
                 score = 0
@@ -125,6 +134,7 @@ while not exit_pressed:
         pipe_list = move_pipes(pipe_list)
         pipe_to_pass_position -= 3
         if is_pipe_passed(pipe_to_pass_position):
+            point_sound.play()
             score += 1 
             pipe_to_pass_position = 1000
 
@@ -140,6 +150,13 @@ while not exit_pressed:
         floor_x -= 1
         if floor_x <= -288:
             floor_x = 0
+    else:
+        title_surface = large_font.render("Flappy Bert", False, (255, 255, 255))
+        title_rect = title_surface.get_rect(center = (144, 180))
+        screen.blit(title_surface, title_rect)
+        description_surface = small_font.render("(press space)", False, (255, 255, 255))
+        description__rect = description_surface.get_rect(center = (144, 230))
+        screen.blit(description_surface, description__rect)
 
     # Draw elements
     draw_pipes(pipe_list)
